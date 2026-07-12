2fbfd9dbc115eb1f81a286b9b51f603858aafd15
Smit Patel <patelsmit090305@gmail.com>
2026-07-12 11:58:25 +0530

    Rename title

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
             <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEndIcon className="size-4" />
             </div>
-            Acme Inc.
+            TransitOps
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
